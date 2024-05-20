0d52a79d2888315339fde47d41d2c6d1e224bf54
lightseaaa <kaizogammer@gmail.com>
2024-05-20 09:37:43 +0700

    login reigster

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_admin'
+    ];
+
 }
